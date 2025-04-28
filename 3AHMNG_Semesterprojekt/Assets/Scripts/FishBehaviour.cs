2ce426ab4c812c3caca6765833892f90b80ee37d
Nici <per210421@spengergasse.at>
2025-04-28 15:49:01 +0200

    Galaxys work now
    Though its in the tester phase (= press 1-4 to switch galaxy)

--- 3AHMNG_Semesterprojekt/Assets/Scripts/FishBehaviour.cs
+++ 3AHMNG_Semesterprojekt/Assets/Scripts/FishBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public enum Galaxy
@@ -13,9 +14,10 @@ public enum Galaxy
 //Combo
 public enum Difficulty
 {
-    Easy,
-    Medium,
-    Hard,
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
     Legendary,
     God
 }
@@ -51,53 +53,183 @@ public class FishBehaviour : MonoBehaviour
     {
         _instance = this;
 
+        //Tutoria
         Fish _stardine = new Fish();
         _stardine._fishCode = 1;
         _stardine._rarity = 1;
         _stardine._name = "Stardine";
-        _stardine._difficulty = Difficulty.Easy;
+        _stardine._difficulty = Difficulty.Common;
         fishDictionary.Add(_stardine._fishCode, _stardine);
 
         Fish _sprayfish = new Fish();
         _sprayfish._fishCode = 2;
         _sprayfish._rarity = 1;
         _sprayfish._name = "Sprayfish";
-        _sprayfish._difficulty = Difficulty.Easy;
+        _sprayfish._difficulty = Difficulty.Common;
         fishDictionary.Add(_sprayfish._fishCode, _sprayfish);
 
         Fish _hemospheel = new Fish();
         _hemospheel._fishCode = 3;
         _hemospheel._rarity = 1;
         _hemospheel._name = "Hemospheel";
-        _hemospheel._difficulty = Difficulty.Easy;
+        _hemospheel._difficulty = Difficulty.Common;
         fishDictionary.Add(_hemospheel._fishCode, _hemospheel);
 
+        //Prehistoria
         Fish _leedsichthys = new Fish();
         _leedsichthys._fishCode = 4;
         _leedsichthys._rarity = 1;
         _leedsichthys._name = "Leedsichthys";
-        _leedsichthys._difficulty = Difficulty.Easy;
+        _leedsichthys._difficulty = Difficulty.Common;
         fishDictionary.Add(_leedsichthys._fishCode, _leedsichthys);
 
         Fish _sacabambaspis = new Fish();
         _sacabambaspis._fishCode = 5;
         _sacabambaspis._rarity = 1;
         _sacabambaspis._name = "Sacabambaspis";
-        _sacabambaspis._difficulty = Difficulty.Medium;
+        _sacabambaspis._difficulty = Difficulty.Common;
         fishDictionary.Add(_sacabambaspis._fishCode, _sacabambaspis);
 
         Fish _horseshoeCrab = new Fish();
         _horseshoeCrab._fishCode = 6;
-        _horseshoeCrab._rarity = 1;
+        _horseshoeCrab._rarity = 2;
         _horseshoeCrab._name = "Horseshoe Crab";
-        _horseshoeCrab._difficulty = Difficulty.Medium;
+        _horseshoeCrab._difficulty = Difficulty.Uncommon;
         fishDictionary.Add(_horseshoeCrab._fishCode, _horseshoeCrab);
+
+        Fish _regretfulFish = new Fish();
+        _regretfulFish._fishCode = 7;
+        _regretfulFish._rarity = 2;
+        _regretfulFish._name = "Regretful Fish";
+        _regretfulFish._difficulty = Difficulty.Uncommon;
+        fishDictionary.Add(_regretfulFish._fishCode, _regretfulFish);
+
+        Fish _helicoprion = new Fish();
+        _helicoprion._fishCode = 8;
+        _helicoprion._rarity = 3;
+        _helicoprion._name = "Helicoprion";
+        _helicoprion._difficulty = Difficulty.Rare;
+        fishDictionary.Add(_helicoprion._fishCode, _helicoprion);
+
+        Fish _kingOssis = new Fish();
+        _kingOssis._fishCode = 9;
+        _kingOssis._rarity = 6;
+        _kingOssis._name = "King Ossis";
+        _kingOssis._difficulty = Difficulty.God;
+        fishDictionary.Add(_kingOssis._fishCode, _kingOssis);
+
+        //Biologica
+        Fish _floralFlounder = new Fish();
+        _floralFlounder._fishCode = 10;
+        _floralFlounder._rarity = 1;
+        _floralFlounder._name = "Floral Flounder";
+        _floralFlounder._difficulty = Difficulty.Common;
+        fishDictionary.Add(_floralFlounder._fishCode, _floralFlounder);
+
+        Fish _honeyCarp = new Fish();
+        _honeyCarp._fishCode = 11;
+        _honeyCarp._rarity = 1;
+        _honeyCarp._name = "Honey Carp";
+        _honeyCarp._difficulty = Difficulty.Common;
+        fishDictionary.Add(_honeyCarp._fishCode, _honeyCarp);
+
+        Fish _bloomster = new Fish();
+        _bloomster._fishCode = 12;
+        _bloomster._rarity = 2;
+        _bloomster._name = "Bloomster";
+        _bloomster._difficulty = Difficulty.Uncommon;
+        fishDictionary.Add(_bloomster._fishCode, _bloomster);
+
+        Fish _grassmuncher = new Fish();
+        _grassmuncher._fishCode = 13;
+        _grassmuncher._rarity = 2;
+        _grassmuncher._name = "Grassmuncher";
+        _grassmuncher._difficulty = Difficulty.Uncommon;
+        fishDictionary.Add(_grassmuncher._fishCode, _grassmuncher);
+
+        Fish _lemonShark = new Fish();
+        _lemonShark._fishCode = 14;
+        _lemonShark._rarity = 3;
+        _lemonShark._name = "Lemon Shark";
+        _lemonShark._difficulty = Difficulty.Rare;
+        fishDictionary.Add(_lemonShark._fishCode, _lemonShark);
+
+        Fish _megaphoton = new Fish();
+        _megaphoton._fishCode = 15;
+        _megaphoton._rarity = 5;
+        _megaphoton._name = "Megaphoton";
+        _megaphoton._difficulty = Difficulty.Legendary;
+        fishDictionary.Add(_megaphoton._fishCode, _megaphoton);
+
+        Fish _queenVitae = new Fish();
+        _queenVitae._fishCode = 16;
+        _queenVitae._rarity = 6;
+        _queenVitae._name = "Queen Vitae";
+        _queenVitae._difficulty = Difficulty.God;
+        fishDictionary.Add(_queenVitae._fishCode, _queenVitae);
+
+        //Galaxia
+        Fish _cosmicCrab = new Fish();
+        _cosmicCrab._fishCode = 17;
+        _cosmicCrab._rarity = 1;
+        _cosmicCrab._name = "Cosmic Crab";
+        _cosmicCrab._difficulty = Difficulty.Common;
+        fishDictionary.Add(_cosmicCrab._fishCode, _cosmicCrab);
+
+        Fish _milkywayWorm = new Fish();
+        _milkywayWorm._fishCode = 18;
+        _milkywayWorm._rarity = 2;
+        _milkywayWorm._name = "Milkyway Worm";
+        _milkywayWorm._difficulty = Difficulty.Uncommon;
+        fishDictionary.Add(_milkywayWorm._fishCode, _milkywayWorm);
+
+        Fish _glider = new Fish();
+        _glider._fishCode = 19;
+        _glider._rarity = 3;
+        _glider._name = "Glider";
+        _glider._difficulty = Difficulty.Rare;
+        fishDictionary.Add(_glider._fishCode, _glider);
+
+        Fish _astralOcta = new Fish();
+        _astralOcta._fishCode = 20;
+        _astralOcta._rarity = 4;
+        _astralOcta._name = "Astral Octa";
+        _astralOcta._difficulty = Difficulty.Epic;
+        fishDictionary.Add(_astralOcta._fishCode, _astralOcta);
+
+        Fish _AEX11_N2 = new Fish();
+        _AEX11_N2._fishCode = 21;
+        _AEX11_N2._rarity = 4;
+        _AEX11_N2._name = "AEX11-N2";
+        _AEX11_N2._difficulty = Difficulty.Epic;
+        fishDictionary.Add(_AEX11_N2._fishCode, _AEX11_N2);
+
+        Fish _magnaLarva = new Fish();
+        _magnaLarva._fishCode = 22;
+        _magnaLarva._rarity = 4;
+        _magnaLarva._name = "Magna Larva";
+        _magnaLarva._difficulty = Difficulty.Epic;
+        fishDictionary.Add(_magnaLarva._fishCode, _magnaLarva);
+
+        Fish _nyanCatfish = new Fish();
+        _nyanCatfish._fishCode = 23;
+        _nyanCatfish._rarity = 5;
+        _nyanCatfish._name = "Nyan Catfish";
+        _nyanCatfish._difficulty = Difficulty.Legendary;
+        fishDictionary.Add(_nyanCatfish._fishCode, _nyanCatfish);
+
+        Fish _ultranova = new Fish();
+        _ultranova._fishCode = 24;
+        _ultranova._rarity = 6;
+        _ultranova._name = "Ultranova";
+        _ultranova._difficulty = Difficulty.God;
+        fishDictionary.Add(_ultranova._fishCode, _ultranova);
     }
 
     // Update is called once per frame
     void Update()
     {
-        galaxy = Galaxy.Tutoria;
+        //galaxy = Galaxy.Tutoria;
 
         
 
